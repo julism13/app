2beabcfacfb367b7b20a929d46cfdeec005c3181
julism13 <111655838+julism13@users.noreply.github.com>
2025-09-21 21:56:11 -0400

    Update constants.h

--- common_src/constants.h
+++ common_src/constants.h
@@ -5,8 +5,8 @@
 
 const uint8_t SEND_USERNAME = 0x01;
 const uint8_t SEND_INITIAL_MONEY = 0x02;
-const uint8_t GET_CURRENT_CAR = 0x03;
-const uint8_t SEND_CURRENT_CAR = 0x04;
+const uint8_t SEND_CURRENT_CAR = 0x03; 
+const uint8_t GET_CURRENT_CAR = 0x04;
 const uint8_t GET_MARKET_INFO = 0x05;
 const uint8_t SEND_MARKET_INFO = 0x06;
 const uint8_t BUY_CAR = 0x07;
@@ -14,4 +14,3 @@ const uint8_t SEND_CAR_BOUGHT = 0x08;
 const uint8_t SEND_ERROR_MESSAGE = 0x09;
 
 #endif
-
